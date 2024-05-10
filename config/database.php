571f534c48d6f590a248a52f7f65407e8927480b
Shreyansh Panchal <shreyansh.panchal14@gmail.com>
2024-05-10 18:58:14 +0530

    fixed: failed to open stream: no such file or directory

---
+++ config/database.php
@@ -0,0 +1,15 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Disable migrations
+    |--------------------------------------------------------------------------
+    |
+    | Prevent Atlas`s default package migrations from running.
+    |
+    */
+    'disable_migrations' => false,
+
+];
--- src/Providers/IntegrationServiceProvider.php
+++ src/Providers/IntegrationServiceProvider.php
@@ -6,15 +6,33 @@ use Illuminate\Support\ServiceProvider;
 
 class IntegrationServiceProvider extends ServiceProvider
 {
+    protected $configFiles = [
+        'views',
+        'routes',
+        'database',
+    ];
+
+    protected $root = __DIR__.'/..';
+
     public function register()
     {
-        //
+        collect($this->configFiles)->each(function ($config) {
+            $this->mergeConfigFrom("{$this->root}/config/$config.php", "atlas.$config");
+        });
     }
 
     public function boot()
     {
-        // $this->loadRoutesFrom(__DIR__.'/../routes/web.php');
-        // $this->loadViewsFrom(__DIR__.'/../resources/views', 'atlas');
-        $this->loadMigrationsFrom(__DIR__.'/../database/migrations');
+        if (! config('atlas.routes.disable_migrations', false)) {
+            $this->loadRoutesFrom(__DIR__.'/../../routes/web.php');
+        }
+
+        if (! config('atlas.views.disable_migrations', false)) {
+            $this->loadViewsFrom(__DIR__.'/../../resources/views', 'atlas');
+        }
+
+        if (! config('atlas.database.disable_migrations', false)) {
+            $this->loadMigrationsFrom(__DIR__.'/../../database/migrations');
+        }
     }
 }
