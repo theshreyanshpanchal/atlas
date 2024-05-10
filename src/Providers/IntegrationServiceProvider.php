1d3e21534566836b0f6618f52e646d0e69e2f6e4
Shreyansh Panchal <shreyansh.panchal14@gmail.com>
2024-05-10 18:50:37 +0530

    integration service provider changes

--- src/Providers/IntegrationServiceProvider.php
+++ src/Providers/IntegrationServiceProvider.php
@@ -13,8 +13,8 @@ class IntegrationServiceProvider extends ServiceProvider
 
     public function boot()
     {
-        $this->loadRoutesFrom(__DIR__.'/../routes/web.php');
-        $this->loadViewsFrom(__DIR__.'/../resources/views', 'atlas');
+        // $this->loadRoutesFrom(__DIR__.'/../routes/web.php');
+        // $this->loadViewsFrom(__DIR__.'/../resources/views', 'atlas');
         $this->loadMigrationsFrom(__DIR__.'/../database/migrations');
     }
 }
