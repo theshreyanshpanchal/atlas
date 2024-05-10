04261771e1831f040eeffaf619a0a822a7b182b3
Shreyansh Panchal <shreyansh.panchal14@gmail.com>
2024-05-10 18:45:45 +0530

    removed config files for now

--- src/Providers/IntegrationServiceProvider.php
+++ src/Providers/IntegrationServiceProvider.php
@@ -6,33 +6,15 @@ use Illuminate\Support\ServiceProvider;
 
 class IntegrationServiceProvider extends ServiceProvider
 {
-    protected $configFiles = [
-        'views',
-        'routes',
-        'database',
-    ];
-
-    protected $root = __DIR__.'/..';
-
     public function register()
     {
-        collect($this->configFiles)->each(function ($config) {
-            $this->mergeConfigFrom("{$this->root}/config/$config.php", "lunar-hub.$config");
-        });
+        //
     }
 
     public function boot()
     {
-        if (! config('atlas.routes.disable_routes', false)) {
-            $this->loadRoutesFrom(__DIR__.'/../routes/web.php');
-        }
-
-        if (! config('atlas.views.disable_views', false)) {
-            $this->loadViewsFrom(__DIR__.'/../resources/views', 'atlas');
-        }
-
-        if (! config('atlas.database.disable_migrations', false)) {
-            $this->loadMigrationsFrom(__DIR__.'/../database/migrations');
-        }
+        $this->loadRoutesFrom(__DIR__.'/../routes/web.php');
+        $this->loadViewsFrom(__DIR__.'/../resources/views', 'atlas');
+        $this->loadMigrationsFrom(__DIR__.'/../database/migrations');
     }
 }
