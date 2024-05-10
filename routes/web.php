9c8e02c2c75436483e7ad53d8bebfc627211a34c
Shreyansh Panchal <shreyansh.panchal14@gmail.com>
2024-05-10 18:29:23 +0530

    require: illuminate/support

--- routes/web.php
+++ routes/web.php
@@ -4,8 +4,8 @@ use Illuminate\Support\Facades\Route;
 
 Route::prefix('atlas')->group(function () {
 
-    Route::get('/', function () { return view('atlas::layouts.splash'); });
+    // Route::get('/', function () { return view('atlas::layouts.splash'); });
 
-    Route::get('/docs', function () { return view('atlas::layouts.docs'); });
+    // Route::get('/docs', function () { return view('atlas::layouts.docs'); });
 
 });
